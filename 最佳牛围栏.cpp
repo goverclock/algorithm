e756b54fdd89be90aacf05f77043cff478ff1924
GomezFEZ <2237261538@qq.com>
2020-12-18 20:05:10 +0800

    Create 最佳牛围栏.cpp

---
+++ 最佳牛围栏.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+const double eps = 1e-5;
+
+int n, f;
+double s[1000010], sum[1000010];
+
+bool C(double x){
+    double ans = -1;
+    double small = 100000000.0;
+    for(int i = 0; i < n; i++) sum[i + 1] = sum[i] + s[i + 1] - x;
+
+    for(int i = f; i <= n; i++){           // 理解这里需要花费一些功夫
+        small = min(small, sum[i - f]);    // 思维还是欠练的
+        ans = max(ans, sum[i] - small);
+    }    
+
+    return ans >= 0;
+}
+
+int main(){
+    cin >> n >> f;
+    for(int i = 1; i <= n; i++) cin >> s[i];
+
+    double l = -1, r = 100000000;
+    while(r > eps + l){
+        double mid = (l + r) / 2.0;
+        if(C(mid)) l = mid;
+        else r = mid;
+    }
+    cout << int(r * 1000) << endl;
+
+    return 0;
+}
