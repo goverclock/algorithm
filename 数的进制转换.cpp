7d6d9bff9f9e73c78b84088c8788d3d1c520b495
GomezFEZ <2237261538@qq.com>
2020-12-29 15:42:53 +0800

    Update 数的进制转换.cpp

--- 数的进制转换.cpp
+++ 数的进制转换.cpp
@@ -30,25 +30,29 @@ int main() {
 
     while (t--) {
         scanf("%d%d%s", &from, &to, A);
-        
+
         idx = 0;
         int len = strlen(A);
         for (int i = len - 1; i >= 0; i--) {  // turn A to A_int
-            if (A[i] >= '0' && A[i] <= '9') A_int[len - i] = A[i] - '0';
-            else if(A[i] >= 'A' && A[i] <= 'Z') A_int[len - i] = A[i] - 'A' + 10;
-            else if(A[i] >= 'a' && A[i] <= 'z') A_int[len - i] = A[i] - 'a' + 36;
+            if (A[i] >= '0' && A[i] <= '9')
+                A_int[len - i] = A[i] - '0';
+            else if (A[i] >= 'A' && A[i] <= 'Z')
+                A_int[len - i] = A[i] - 'A' + 10;
+            else if (A[i] >= 'a' && A[i] <= 'z')
+                A_int[len - i] = A[i] - 'a' + 36;
         }
 
-        while (len = div1(to, len)){
-            ans[++idx] = A_int[0] / from;
-        }
+        while (len = div1(to, len)) ans[++idx] = A_int[0] / from;
         ans[++idx] = A_int[0] / from;
 
         printf("%d %s\n%d ", from, A, to);
-        for (int i = idx; i >= 1; i--){
-            if (ans[i] >= 0 && ans[i] < 10) printf("%d", ans[i]);
-            else if(ans[i] >= 10 && ans[i] <= 35) printf("%c", ans[i] + 'A' - 10);
-            else if(ans[i] >= 36 && ans[i] <= 61) printf("%c", ans[i] + 'a' - 36);
+        for (int i = idx; i >= 1; i--) {
+            if (ans[i] >= 0 && ans[i] < 10)
+                printf("%d", ans[i]);
+            else if (ans[i] >= 10 && ans[i] <= 35)
+                printf("%c", ans[i] + 'A' - 10);
+            else if (ans[i] >= 36 && ans[i] <= 61)
+                printf("%c", ans[i] + 'a' - 36);
         }
         puts("\n");
     }
